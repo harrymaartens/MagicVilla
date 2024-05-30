7829cf6ea451b64c4194c6348a2e404c855764c0
Harry Maartens <hmaartens@hotmail.com>
2024-05-30 17:24:31 +0200

    HttpGet endpoint

---
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        // Dit is een HttpGet endpoint
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Pool View" },
+                new Villa { Id = 2, Name = "Beach View" }
+            };            
+        }
+    }
+}
