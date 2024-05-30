0c991086d1b266b9872ec62be3450a93522ed774
Harry Maartens <hmaartens@hotmail.com>
2024-05-30 18:03:49 +0200

    Section 2: First End point

--- MagicVilla_API/Controllers/VillaAPIController.cs
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -1,21 +1,27 @@
-﻿using MagicVilla_API.Models;
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_API.Controllers
 {
+    //[Route("api/[controller]")]
     [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         // Dit is een HttpGet endpoint
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa>
-            {
-                new Villa { Id = 1, Name = "Pool View" },
-                new Villa { Id = 2, Name = "Beach View" }
-            };            
+            return VillaStore.villaList;
+        }
+
+        // Voor een enkele villa is een id verplicht
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
         }
     }
 }
