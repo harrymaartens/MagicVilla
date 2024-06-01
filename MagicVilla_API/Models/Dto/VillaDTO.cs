4d0d2169cc00b94ad42415577281e8216cfcf8c1
Harry Maartens <hmaartens@hotmail.com>
2024-06-01 15:22:56 +0200

    Section 3: CRUD Functionality

--- MagicVilla_API/Models/Dto/VillaDTO.cs
+++ MagicVilla_API/Models/Dto/VillaDTO.cs
@@ -1,8 +1,19 @@
-﻿namespace MagicVilla_API.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_API.Models.Dto
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
+
+        // Bezetting
+        public int Occupancy { get; set; }
+
+        // Square feet = Vierkante meter
+        public int Sqft { get; set; }
     }
 }
