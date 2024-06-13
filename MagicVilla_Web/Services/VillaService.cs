9b36d449f45fc079c0a9d97c61cdfb0aeabd8a1f
Harry Maartens <hmaartens@hotmail.com>
2024-06-13 15:14:02 +0200

    Section 7: Alles werkt t/m Delete Villa, Update issue is ook opgelost.

--- MagicVilla_Web/Services/VillaService.cs
+++ MagicVilla_Web/Services/VillaService.cs
@@ -36,7 +36,7 @@ namespace MagicVilla_Web.Services
             return SendAsync<T>(new APIRequest()
             {
                 ApiType = SD.ApiType.DELETE,
-                Url = villaUrl + "/api/villaAPI" + id
+                Url = villaUrl + "/api/villaAPI/" + id
             });
         }
 
@@ -54,7 +54,7 @@ namespace MagicVilla_Web.Services
             return SendAsync<T>(new APIRequest()
             {
                 ApiType = SD.ApiType.GET,
-                Url = villaUrl + "/api/villaAPI" + id
+                Url = villaUrl + "/api/villaAPI/" + id
             });
         }
 
