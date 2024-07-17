6e2c2e17148a3baba558e1fca33159b0bbda9223
Harry Maartens <hmaartens@hotmail.com>
2024-07-17 17:33:44 +0200

    Section 10 - API Version Configuration

--- MagicVilla_API/Controllers/VillaAPIController.cs
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -14,8 +14,9 @@ using System.Net;
 namespace MagicVilla_API.Controllers
 {
     //[Route("api/[controller]")]
-    [Route("api/VillaAPI")]
+    [Route("api/v{version:apiVersion}/VillaAPI")]
     [ApiController]
+    [ApiVersion("1.0")]
     public class VillaAPIController : ControllerBase
     {
         // Alle ReturnTypes worden nu _response
