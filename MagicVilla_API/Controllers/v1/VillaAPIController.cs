385b8a8064d2d7c8c8db0ac8c615560e0031722c
Harry Maartens <hmaartens@hotmail.com>
2024-07-23 14:38:44 +0200

    Section 11: Caching, Filter en Pagination

--- MagicVilla_API/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_API/Controllers/v1/VillaAPIController.cs
@@ -39,7 +39,7 @@ namespace MagicVilla_API.Controllers.v1
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")] int? occupancy, 
-            [FromQuery] string? search, int pageSize = 2, int pageNumber = 1)
+            [FromQuery] string? search, int pageSize = 0, int pageNumber = 1)
         {
             try
             {
