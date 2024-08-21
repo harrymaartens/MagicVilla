083f5b3c69cda6411c631f4518cccc0a527ef755
Harry Maartens <hmaartens@hotmail.com>
2024-08-21 13:16:50 +0200

    Section 9: Customize Handler by Exception

--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,5 +1,6 @@
 using MagicVilla_API;
 using MagicVilla_API.Data;
+using MagicVilla_API.Extensions;
 using MagicVilla_API.Filters;
 using MagicVilla_API.Models;
 using MagicVilla_API.Repository;
@@ -101,36 +102,7 @@ else
 
 //app.UseExceptionHandler("/ErrorHandling/ProcessError");
 
-app.UseExceptionHandler(error =>
-{
-    error.Run(async context =>
-    {
-        context.Response.StatusCode = 500;
-        context.Response.ContentType = "application/json";
-        var feature = context.Features.Get<IExceptionHandlerFeature>();
-        if (feature != null)
-        {
-            if (app.Environment.IsDevelopment())
-            {
-                await context.Response.WriteAsync(JsonConvert.SerializeObject(new
-                {
-                    StatusCode = context.Response.StatusCode,
-                    ErrorMessage = feature.Error.Message,
-                    StackTrace = feature.Error.StackTrace
-                }));
-            }
-            else
-            {
-                await context.Response.WriteAsync(JsonConvert.SerializeObject(new
-                {
-                    Statuscode = context.Response.StatusCode,
-                    ErrorMessage = "Hello From Program.cs Exception Handler"
-                }));
-            }
-        }
-    });
-});
-
+app.HandleError(app.Environment.IsDevelopment());
 
 app.UseStaticFiles();
 app.UseHttpsRedirection();
